f155e6bcc6a4229cd391add987a8eb01ea81a95b
Artur Todeschni Crestani <artodeschini@gmail.com>
2023-03-17 20:32:42 -0300

    aicionado model tarefas

---
+++ src/app/tarefas/shared/tarefa.model.ts
@@ -0,0 +1,9 @@
+export class Tarefa {
+
+    // defino minha classe e seu atributos o ? diz que sao opcionais
+    constructor(
+        public id?: number,
+        public nome?: string,
+        public concluida?: boolean) {}
+
+}
